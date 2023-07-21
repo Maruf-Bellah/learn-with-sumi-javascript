87b063ba918f548db50f7028013f472e3c5f44a4
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-22 03:06:21 +0600

    arithmetic finish

---
+++ Beginer/arithmetic.js
@@ -0,0 +1,47 @@
+// Arithmetic operator হলো যে গুলো যোগ বিয়োগ করে 
+
+// Operatiors and Operands 
+// Operatior বলে + - * / কে 
+// Operands বলে সংখ্যা কে ৫৬ + ৬৪
+/* 
+
+var x = 5;
+var y = 2;
+var z = x + y;
+
+console.log(z);
+var x = 5;
+var y = 2;
+var z = x - y;
+
+console.log(z);
+var x = 5;
+var y = 2;
+var z = x * y;
+
+console.log(z);
+var x = 5;
+var y = 2;
+var z = x / y;
+
+console.log(z);
+var x = 5;
+var y = 2;
+var z = x % y;
+
+console.log(z);
+
+var x = 5;
+x--;
+var z = x;
+console.log(z);
+
+for(i = 10; i > 0; i--){
+    console.log(i);
+} */
+/* 
+let x = 5;
+let z = Math.pow(x , 2) */
+
+let x = 100 + 50 * 3;
+console.log(x);
