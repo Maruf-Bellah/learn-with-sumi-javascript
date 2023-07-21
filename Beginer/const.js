87e3a5728e2982e3484d55c30b5777f71e27a5a7
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-22 02:05:45 +0600

    const finish

---
+++ Beginer/const.js
@@ -0,0 +1,24 @@
+// const resing kora jay na 
+// const redeclear kora jay na 
+
+
+// tobe array o object er moddhe const er valu update korte o add kora jay 
+
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+cars[3] = "Toyota";
+cars.push("Audi")
+cars.pop()
+
+// console.log(cars);
+
+const car = {
+    type: "Fiat",
+    model: "500",
+    color: "white"
+};
+car.color = "black"
+car.type = "aboul"
+console.log(car);
+
