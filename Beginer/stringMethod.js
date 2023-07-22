268ce6322602c487a00f5dcce23648d7dbae2351
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-22 19:50:22 +0600

    string method start

---
+++ Beginer/stringMethod.js
@@ -0,0 +1,32 @@
+// // String Method 
+
+
+ 
+// String length  পুরা জায়গায় সংখ্যা দেখায় 
+
+
+// // Extracting String Parts  কোন জায়গা থেকে কোন জায়গা পর্যন্ত বাহির করবে 
+
+
+// String slice(start, end)
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part);
+
+// String substring()
+// String substr()
+
+
+// String replace()
+// String replaceAll()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String charAt()
+// String charCodeAt()
+// String split() 
