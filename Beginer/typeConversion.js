22bb2a40ce2a738618773285e45a69e7fa4b3381
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-25 03:28:29 +0600

    typeOf Conversion finish

---
+++ Beginer/typeConversion.js
@@ -0,0 +1,49 @@
+// conversion হলো একটা থেকে অন্য টায় convert করা যায় 
+
+
+// Converting Strings to Numbers
+// Converting Numbers to Strings
+// Converting Dates to Numbers
+// Converting Numbers to Dates
+// Converting Booleans to Numbers
+// Converting Numbers to Booleans
+
+
+// JavaScript Type Conversion======================
+
+// ১= নিজেরাই করতে পারে
+// ২= নিজে নিজে করে নেয় 
+
+
+// Number Methods==========================================
+
+// Number()	Returns a number, converted from its argument
+// parseFloat()	Parses a string and returns a floating point number
+// parseInt()	Parses a string and returns an integer
+
+
+// The Unary + Operator==========================================
+
+// The unary + operator can be used to convert a variable to a number:
+
+// let y = "5";      // y is a string
+// let x = + y;      // x is a number
+
+// ==============================================
+
+// let y = "John";   // y is a string
+// let x = + y;      // x is a number (NaN)
+
+
+
+
+
+// Automatic Type Conversion===============================
+
+/* 
+5 + null    // returns 5         because null is converted to 0
+"5" + null  // returns "5null"   because null is converted to "null"
+"5" + 2     // returns "52"      because 2 is converted to "2"
+"5" - 2     // returns 3         because "5" is converted to 5
+"5" * "2"   // returns 10        because "5" and "2" are converted to 5 and 2
+ */
