379f259f3e66c868eac2d7d0abf2cdfe14648f85
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-27 03:08:55 +0600

    class learn in js

--- Beginer/classe.js
+++ Beginer/classe.js
@@ -1,4 +1,17 @@
 // class ekhon kicui buji nai pore abar valo moto dekhe buje nibe 
 
-
+class Car {
+    constructor(name , year){
+        this.name = name;
+        this.year = year;
+    }
+    age(){
+        const dete = new Date();
+        return dete.getFullYear() - this.year;
+    }
+}
  
+const myCar = new Car("Food" , 2005);
+const myCar1 = new Car("Food" , 2005);
+
+console.log(Car);
