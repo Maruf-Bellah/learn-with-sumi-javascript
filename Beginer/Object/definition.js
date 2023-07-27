9c9d6e131a429cdd6ffea4c48e38afff26e641ae
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-27 12:45:52 +0600

    object start

---
+++ Beginer/Object/definition.js
@@ -0,0 +1,40 @@
+// In JavaScript, objects are king. If you understand objects, you understand JavaScript.
+
+
+// JavaScript defines 7 types of primitive data types:
+
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+// bigint
+
+
+const person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+
+
+const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+
+const x = person;
+x.age = 10;
+console.log(person);
+console.log(x);
+// console.log(person1);
+
+
+// empty object create kore pore add kora jay 
+
+const mans = {};
+mans.firstName = "maruf";
+mans.lastName = "bellah";
+mans.age = 20;
+mans.eyeColor = "black";
+
+// console.log(mans);
