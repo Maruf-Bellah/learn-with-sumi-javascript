c50acbc42647db377aab853a791a0befdf8bde58
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-07-29 15:00:57 +0600

    maps is finis

---
+++ Beginer/Object/maps.js
@@ -0,0 +1,53 @@
+// map key value pairs কে ধারন করে আর সেই key যে কোন datatype হতে পারে 
+
+
+// How to Create a Map
+// You can create a JavaScript Map by:========================
+
+// Passing an Array to new Map()
+// Create a Map and use Map.set()
+
+const fruits = new Map([
+    ["apple", 500],
+    ["orange", 600],
+    ["pain apple", 300],
+    ["kiwi", 800],
+])
+
+// console.log(fruits);
+
+// Map.set()=========================
+
+const fruits1 = new Map();
+
+fruits1.set("apple", 200);
+fruits1.set("orange", 300);
+fruits1.set("pain apple", 400);
+fruits1.set("kiwi", 800);
+fruits1.set("mango", 1000);
+
+// set() method value ও change করতে ও পারে =======================
+fruits1.set("apple", 100000);
+
+// get() method key এর value পাওয়ার জন্য use করা হয় ====================
+const show = fruits1.get("apple")
+// size property element এর নাম্বার টা দেন  ====================
+const show1 =fruits1.size;
+
+// delete() method একটি map element কে delete করে
+// fruits1.delete('apple')
+
+// clear() method map থেকে সব Element remove করে দেয় 
+// fruits1.clear();
+
+
+// has() method দেখে map এর মধ্যে আছে কি নাই , যদি থাকে true না থাকলে false দেখাবে 
+
+const show4 = fruits1.has('apple')
+
+// instanceof return the true ===================
+console.log(fruits1 instanceof Map);
+
+
+// console.log(fruits1);
+// console.log(show4);
