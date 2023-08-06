4bbbf0b65a3826b599843fe3dda9ad85971876f5
Maruf-Bellah <mdmaruf5511507@gmail.com>
2023-08-06 12:03:46 +0600

    dome method finish

---
+++ Beginer/DOM/dom.js
@@ -0,0 +1,23 @@
+// DOM = document object model 
+
+// POEM 
+// P=propertise 
+// O=object 
+// E= event 
+// M=method 
+
+// Dom is a Document Object Model & Programming Interface for HTML that defines HTML object, properties, methods & events to get , change , and or delete HTML element 
+
+
+
+// ======================== javascript ja ja korte par ====================
+
+// JavaScript can change all the HTML elements in the page
+// JavaScript can change all the HTML attributes in the page
+// JavaScript can change all the CSS styles in the page
+// JavaScript can remove existing HTML elements and attributes
+// JavaScript can add new HTML elements and attributes
+// JavaScript can react to all existing HTML events in the page
+// JavaScript can create new HTML events in the page
+
+
---
+++ Beginer/DOM/methods.js
@@ -0,0 +1,5 @@
+// change the html element 
+
+document.getElementById('demo').innerHTML="Assalamu alaikum";
+
+document.getElementById('demo1').innerHTML="hey bro how are you"
