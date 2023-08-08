a88940e23b112c3b7c124495b62877241df8744b
unknown <mdmruf5511507@gmail.com>
2023-08-08 15:44:39 +0600

    html is push

---
+++ Beginer/DOM/html.js
@@ -0,0 +1,20 @@
+
+// changin the html element 
+
+// var show = document.getElementById('hello');
+// show.innerHTML = 'Tomake chai ami aro kache '
+
+
+// chang the image 
+
+document.getElementById('funny').src='./funny2.jpg';
+
+const funny = document.getElementById('funny');
+// funny.src='./funny2.jpg'
+
+
+// dynamic HTML content 
+
+document.querySelector('h1#hello').innerHTML = Date();
+
+document.write(Date())
