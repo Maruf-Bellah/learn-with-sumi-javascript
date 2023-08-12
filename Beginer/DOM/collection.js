0dd00806aea4914884ff3343f0974e64d2cfb85e
marufbellah <90157254+Maruf-Bellah@users.noreply.github.com>
2023-08-12 23:36:27 +0600

    somthin added

---
+++ Beginer/DOM/collection.js
@@ -0,0 +1,3 @@
+// const myCollection = document.getElementsByTagName("p");
+// document.getElementById("hello").innerHTML ="hello bro whats up" + myCollection.length + 'paragraph';
+console.log('hello bro63');
