15a92264797cf3e08ec168ea4e03770c8b2e73fb
maruf bellah <mdmaruf5511507@gmail.com>
2024-02-27 23:45:05 +0600

    boject is learning

--- Beginer/Object/7_prototype.js
+++ Beginer/Object/7_prototype.js
@@ -1,17 +1,25 @@
-// কোন একটা object এর prototype এর মধ্যে যদি কিছু দিতে পারি তাহলে সেটা সমস্ত object এর জন্য available হয়ে যাবে 
+// কোন একটা object এর prototype এর মধ্যে যদি কিছু দিতে পারি তাহলে সেটা সমস্ত object এর জন্য available হয়ে যাবে
 
+function Person(first, last, age) {
+  (this.fistName = first),
+    (this.lastName = last),
+    (this.age = age),
+    (this.fullName = function () {
+      return this.fistName + " " + this.lastName + " " + this.age;
+    });
+}
 
-function Person(first, last, age){
-    this.fistName = first,
-    this.lastName = last,
-    this.age = age ,
-    this.fullName = function(){
-        return this.fistName + ' ' + this.lastName + ' ' + this.age;
-    }
-};
+const maruf = new Person("Maruf", "Bellah", 20);
 
-const maruf = new Person('Maruf', 'Bellah', 20);
+Person.prototype.nationality = "Bangladesh";
+// console.dir(Person);
 
+function Person2(first, last, age, eyeColor) {
+  this.fistName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyeColor;
+}
 
-Person.prototype.nationality ="Bangladesh";
-console.dir(Person);
+let name = new Person2("maruf", "bellah", 23, "black");
+console.log(name);
