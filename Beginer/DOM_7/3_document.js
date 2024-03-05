92f9a3ed651432d063b40027d8e33583cc40b95b
maruf bellah <mdmaruf5511507@gmail.com>
2024-03-05 13:28:51 +0600

    dom is learning

---
+++ Beginer/DOM_7/3_document.js
@@ -0,0 +1,25 @@
+// HTML DOM ডকুমেন্ট অবজেক্ট হল আপনার ওয়েব পেজের অন্য সব অবজেক্টের মালিক।
+
+// Finding HTML Elements=======================================
+// document.getElementById(id)	Find an element by element id
+// document.getElementsByTagName(name)	Find elements by tag name
+// document.getElementsByClassName(name)	Find elements by class name
+
+// Changing HTML Elements=========================================
+
+// element.innerHTML =  new html content	Change the inner HTML of an element
+// element.attribute = new value	Change the attribute value of an HTML element
+// element.style.property = new style	Change the style of an HTML element
+// Method	Description
+// element.setAttribute(attribute, value)	Change the attribute value of an HTML element
+
+// Adding and Deleting Elements====================================
+
+// document.createElement(element)	Create an HTML element
+// document.removeChild(element)	Remove an HTML element
+// document.appendChild(element)	Add an HTML element
+// document.replaceChild(new, old)	Replace an HTML element
+// document.write(text)	Write into the HTML output stream
+
+// Adding Events Handlers==================================
+// document.getElementById(id).onclick = function(){code}	Adding event handler code to an onclick event
---
+++ Beginer/DOM_7/4_element.js
@@ -0,0 +1,12 @@
+// add and changing ============================
+
+const show = (document.getElementById("intro").innerHTML =
+  "hello how are you bro");
+
+// Finding Element ================================
+var hey = (document.getElementsByTagName("p").innerHTML =
+  "Amar Sonar Bangla Ami Tomay Valobashi");
+
+document.getElementById("intro").innerHTML = hey;
+
+// finding Element with class =========================
