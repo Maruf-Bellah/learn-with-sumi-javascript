8e8ecdcab02d3285482befb9c7c7a8166a699cff
maruf bellah <mdmaruf5511507@gmail.com>
2024-03-13 00:06:32 +0600

    cookies is learning

---
+++ Beginer/BOM_8/5_navigator.js
@@ -0,0 +1,9 @@
+// window.navigator অবজেক্টে ভিজিটরের ব্রাউজার সম্পর্কে তথ্য থাকে।
+
+// window.navigator অবজেক্টটি উইন্ডো প্রিফিক্স ছাড়াই লেখা যেতে পারে।
+
+// কিছু উদাহরণ:
+
+// navigator.cookieEnabled
+// navigator.appCodeName
+// navigator.platform
